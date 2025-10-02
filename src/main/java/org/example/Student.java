0de50c8202a6b89eb92642d87b1d889e1cdfacd5
Nicole0509 <niashavril0509@gmail.com>
2025-10-02 12:08:39 +0200

    Created a construstor to initialize values and add Student function

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Student {
 
     private String firstName;
@@ -37,5 +41,27 @@ public class Student {
 
     public void setDateOfBirth(String dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
+    }
+
+    public  Student(String firstName, String lastName, String email, String dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void addStudent(Connection connection) throws SQLException {
+
+        String query = "INSERT INTO students VALUES (?, ?, ?, ?)";
+        PreparedStatement statement = connection.prepareStatement(query);
+
+        statement.setString(1, firstName);
+        statement.setString(2, lastName);
+        statement.setString(3, email);
+        statement.setString(4, dateOfBirth);
+        statement.executeUpdate();
+
+        statement.close();
+
     }
 }
