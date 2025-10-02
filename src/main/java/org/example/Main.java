d1317f3afe3ae833458db83d2aae89400f0464e3
Nicole0509 <niashavril0509@gmail.com>
2025-10-02 11:58:45 +0200

    Added a connection to the DB and it was sucessful

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    static String URL = "jdbc:postgresql://localhost:5432/postgres?currentSchema=public";
+    static String USER = "postgres";
+    static String PASSWORD = "Nicole0509";
+
+    public static void main(String[] args) throws SQLException {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Student Management with Java\n");
+
+        Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+
+    }
+}
